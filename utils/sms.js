80245c36a6187bcf91629a4228283f09d18fc41e
lutheralien <eyandilutherking2003@gmail.com>
2024-09-30 16:00:50 +0100

    nalo

--- utils/sms.js
+++ utils/sms.js
@@ -1,24 +1,32 @@
 const axios = require('axios');
 
 async function sendSMS(phoneNumber, message) {
-    const key = 'dDaK07CbVFx1C3oQ12JcLlxcp';
-    const sender_id = 'esopht';
-    const url = 'https://apps.mnotify.net/smsapi';
+    const username = "esopht";
+    const password = "youroge1968";
+    const sender_id = "esopht";
+    const url = "https://sms.nalosolutions.com/smsbackend/clientapi/Resl_Nalo/send-message/";
 
-    const params = new URLSearchParams({
-        key,
-        to: phoneNumber,
-        msg: message,
-        sender_id
-    });
+    const body = {
+        msisdn: phoneNumber,
+        sender_id: sender_id,
+        message: message,
+        username: username,
+        password: password
+    };
 
     try {
-        const response = await axios.get(`${url}?${params.toString()}`);
-        if (response.data.status === 'success' && response.data.code === '1000') {
-            console.log('SMS sent successfully:', response.data.message);
+        const response = await axios.post(url, body, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+            console.log(response);
+            
+        if (response.status === 200) {
+            console.log('SMS sent successfully');
             return true;
         } else {
-            console.error('Failed to send SMS:', (response.data.message || "Invalid Number"));
+            console.error('Failed to send SMS:', response.data);
             return false;
         }
     } catch (error) {
@@ -26,10 +34,8 @@ async function sendSMS(phoneNumber, message) {
         return false;
     }
 }
-
-// Example usage:
-// sendSMS("1550748724", "Your OTP is 1").then(res => console.log(res)).catch(err => console.log(err));
-
+// sendSMS("020748724177","hi").then((res)=>console.log(res)).catch((err)=>console.log(err)
+// )
 module.exports = {
     sendSMS
 };
